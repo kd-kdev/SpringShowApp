0519fa438fee659e9b71f46b1dade129c6ab26e5
kd-kdev <47572235+kd-kdev@users.noreply.github.com>
2025-05-15 11:18:54 +0200

    Initial commit

---
+++ ShowApp/src/main/java/com/kd/ShowApp/ShowAppApplication.java
@@ -0,0 +1,13 @@
+package com.kd.ShowApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowAppApplication.class, args);
+	}
+
+}
---
+++ ShowApp/src/test/java/com/kd/ShowApp/ShowAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kd.ShowApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
